691d232748227f8bd88c535863eacb99f61e8ae8
moodrain <moerain@qq.com>
2020-06-29 11:02:09 +0800

    first commit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $key = request('genKey') ? encrypt(config('app.api_key')) : null;
+        $key = request('genKey') ? encrypt(config('app.api_token')) : null;
         return $this->view('index', ['key' => $key]);
     }
 }
