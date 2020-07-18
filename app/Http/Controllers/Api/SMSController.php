ec87db62916d873a0ad5b6f428d15336b22f28fc
moodrain <moerain@qq.com>
2020-07-18 16:22:30 +0800

    first commit

--- app/Http/Controllers/Api/SMSController.php
+++ app/Http/Controllers/Api/SMSController.php
@@ -18,7 +18,7 @@ class SMSController extends Controller
         $this->initAliClient();
         $this->validate(request(), [
             'code' => 'int|between:1000,9999',
-            'phone' => ['regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/'],
+            'phone' => ['phone', 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/'],
             'applicationId' => 'exists:applications,id',
         ]);
         $code = request('code') ?? mt_rand(1000, 9999);
